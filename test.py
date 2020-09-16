541094a827de0c0bf8d7863627ce3797d869a0f3
Devansh Sharma <42490656+devansh9011@users.noreply.github.com>
2020-09-17 01:01:54 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,31 @@
+import cv2
+cap=cv2.VideoCapture(0)
+cap.set(3,640)
+cap.set(4,480)
+cap.set(10,100)
+while True:
+    ret,frame=cap.read()
+    if ret is False:
+        break
+    roi=frame [50:240,:380]
+    rows,cols,_=roi.shape
+    gray_roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+    gray_roi=cv2.GaussianBlur(gray_roi,(7,7),0)
+    _,threshold=cv2.threshold(gray_roi,3,255,cv2.THRESH_BINARY_INV)
+    contours,_=cv2.findContours(threshold,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    contours=sorted(contours,key=lambda x:cv2.contourArea(x),reverse=True)
+    for cnt in contours:
+        (x,y,w,h)=cv2.boundingRect(cnt)
+        #cv2.drawContours(roi,[cnt],-1,(0,0,255),3)
+        cv2.rectangle(roi,(x,y),(x+w,y+h),(255,0,0),2)
+        cv2.line(roi,(x+int(w/2),0),(x+int(w/2),rows),(0,255,0),2)
+        cv2.line(roi,(0,y+int(h/2)),(cols,y+int(h/2)),(0,255,0),2)
+        break
+    cv2.imshow("thr",threshold)
+    cv2.imshow("Grayroi",gray_roi)
+    cv2.imshow("roi",roi)
+    key=cv2.waitKey(30)
+    if key==27:
+        break
+
+cv2.destroyAllWindows()
