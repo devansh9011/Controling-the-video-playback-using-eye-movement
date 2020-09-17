ccb7becfeece48028ae4bede452ba29c2a687076
Devansh Sharma <42490656+devansh9011@users.noreply.github.com>
2020-09-17 19:34:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,55 +4,59 @@ import matplotlib.pyplot as plt
 
 #%matplotlib inline
 
-face_cascade = cv2.CascadeClassifier(r"F:\Project_Data\haarcascade_frontalface_default.xml")
-eye_cascade=cv2.CascadeClassifier(r"F:\Project_Data\haarcascade_eye.xml")
+face_cascade = cv2.CascadeClassifier("/home/devil/PycharmProjects/venv/lib/python3.8/site-packages/cv2/data/haarcascade_frontalface_default.xml")
+eye_cascade = cv2.CascadeClassifier('/home/devil/PycharmProjects/venv/lib/python3.8/site-packages/cv2/data/haarcascade_eye.xml')
+#smile_cascade = cv2.CascadeClassifier('/home/devil/PycharmProjects/venv/lib/python3.8/site-packages/cv2/data/haarcascade_smile.xml')
+
 
 def detect_face(img):
-    face_img=img.copy()
-    face_rectangle = face_cascade.detectMultiScale(face_img)
-    xt=yt=yb=xb=0
-    for (x,y,w,h) in face_rectangle:
-        xt=x
-        yt=y
-        xb=x+w
-        yb=y+h
-        cv2.rectangle(face_img,(x,y),(x+w,y+h),(255,255,255),5)
-        break
-    eyes_rectangle = eye_cascade.detectMultiScale(face_img)
-    for (x,y,w,h) in eyes_rectangle:
-        if x>=xt and y>=yt and x<=xb and y<=yb and x+w<=xb and y+h<=yb:
-            cv2.rectangle(face_img, (x, y), (x + w, y + h), (255, 0, 0), 5)
-        break
+    face_img = img.copy()
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    cv2.imshow('gray', gray)
+    face_rectangle = face_cascade.detectMultiScale(face_img, scaleFactor=1.4, minNeighbors=5)
+    for (x, y, w, h) in face_rectangle:
+        cv2.rectangle(face_img, (x, y), (x + w, y + h), (255, 255, 255), 5)
+        face = img[y:y+h, x:x+w]
+        eyes_rectangle = eye_cascade.detectMultiScale(face)
+        for (xx, yy, ww, hh) in eyes_rectangle:
+            cv2.rectangle(face_img, (xx+x, y+yy), (x+xx + ww, y+yy + hh), (255, 0, 0), 2)
+
     return face_img
 
-cap= cv2.VideoCapture(0)
+
+cap = cv2.VideoCapture(0)
 
 while True:
-    ret, frame =cap.read()
+    ret, frame = cap.read()
     frame = detect_face(frame)
     cv2.imshow('face detect', frame)
-    if cv2.waitKey(5) & 0xFF ==27:
+    if cv2.waitKey(5) & 0xFF == 27:
         break
 cap.release()
 cv2.destroyAllWindows()
 
 """
 # opening the camera and capturing a video and saving it in the drive
+
 # Capturing the video from the camera
 cap = cv2.VideoCapture(0)
+
 # width and height of the frame
 width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
 output = cv2.VideoWriter('myvideo.mkv',
                          cv2.VideoWriter_fourcc(*'XVID'),
                          20,
                          (width, height))
+
 while True:
     ret, frame = cap.read()
     output.write(frame)
     cv2.imshow('frame', frame)
     if cv2.waitKey(5) & 0xFF ==27:
         break
+
 cap.release()
 output.release()
 cv2.destroyAllWindows()
@@ -60,18 +64,23 @@ cv2.destroyAllWindows()
 
 """
 #to display circle by taking center interactively from the user
+
 def draw_circle(event, x, y, flags, param):
+
     global center, clicked
     if event == cv2.EVENT_LBUTTONDOWN:
         center=(x,y)
         clicked=False
     if event == cv2.EVENT_LBUTTONUP:
         clicked =True
+
 center =(0,0)
 clicked = False
+
 cap= cv2.VideoCapture(0)
 cv2.namedWindow('testing')
 cv2.setMouseCallback('testing',draw_circle)
+
 while True:
     ret, frame =cap.read()
     if clicked:
@@ -79,6 +88,7 @@ while True:
     cv2.imshow('frame',frame)
     if cv2.waitKey(5) & 0xFF == 27:
         break
+
 cap.release()
 cv2.destroyAllWindows()
 """
