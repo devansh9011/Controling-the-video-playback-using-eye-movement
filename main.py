ec66dd35c8a4cf1ef62483f02d48282248670c41
Devansh Sharma <devansh9011@gmail.com>
2020-12-01 13:12:01 +0530

    Corrected

--- main.py
+++ main.py
@@ -8,43 +8,9 @@ face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
 
-class CountsPerSec:
-    """
-    Class that tracks the number of occurrences ("counts") of an
-    arbitrary event and returns the frequency in occurrences
-    (counts) per second. The caller must increment the count.
-    """
-
-    def __init__(self):
-        self._start_time = None
-        self._num_occurrences = 0
-
-    def start(self):
-        self._start_time = datetime.now()
-        return self
-
-    def increment(self):
-        self._num_occurrences += 1
-
-    def countsPerSec(self):
-        elapsed_time = (datetime.now() - self._start_time).total_seconds()
-        return self._num_occurrences / elapsed_time
-
-
-def putIterationsPerSec(frame, iterations_per_sec):
-    """
-    Add iterations per second text to lower-left corner of a frame.
-    """
-
-    cv2.putText(frame, "{:.0f} iterations/sec".format(iterations_per_sec),
-        (10, 450), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255))
-    return frame
-
-
 class webcam:
     def __init__(self):
         self.cap = cv2.VideoCapture(0)
-        self.cps = CountsPerSec().start()
 
     def detect_face(self):
         ret, frame = self.cap.read()
@@ -59,9 +25,8 @@ class webcam:
             eyes_rectangle = eye_cascade.detectMultiScale(face)  # detection for eyes in the face
             for (xx, yy, ww, hh) in eyes_rectangle:
                 cv2.rectangle(face_img, (xx + x, y + yy), (x + xx + ww, y + yy + hh), (255, 0, 0), 2)  # drawing rectangle over the detected eyes
-        face_img = putIterationsPerSec(face_img, self.cps.countsPerSec())
+
         cv2.imshow('face detect', face_img)
-        self.cps.increment()
         return detected
 
     def __del__(self):
@@ -73,13 +38,10 @@ class playvideo:
         self.movie = cv2.VideoCapture(path)
         self.fps = self.movie.get(cv2.CAP_PROP_FPS)
         self.play = True
-        self.cps = CountsPerSec().start()
 
     def playthevideo(self):
         ret, frame = self.movie.read()
-        frame = putIterationsPerSec(frame, self.cps.countsPerSec())
         cv2.imshow('movie', frame)
-        self.cps.increment()
 
     def __del__(self):
         self.movie.release()
@@ -94,8 +56,7 @@ def foo(path):
 
     global seconds
     while True:
-        #flag = wb.detect_face()
-        flag=True
+        flag = wb.detect_face()
         if flag and mv.play:
             mv.playthevideo()
             seconds = time.time()
