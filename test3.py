6e27bea46f42f3f823487a55aa0db97b458de726
Devansh Sharma <devansh9011@gmail.com>
2020-12-01 12:27:37 +0530

    added the video player in test3.py

--- test3.py
+++ test3.py
@@ -15,23 +15,23 @@ class ApplicationWindow(Gtk.Window):
         self.is_player_active = False
         self.connect("destroy", Gtk.main_quit)
 
+        # Creating media playback buttons
+        self.playback_button = Gtk.Button()
+        self.stop_button = Gtk.Button()
+
+        # initialising the images
+        self.play_image = Gtk.Image.new_from_icon_name("gtk-media-play", Gtk.IconSize.MENU)
+        self.pause_image = Gtk.Image.new_from_icon_name("gtk-media-pause", Gtk.IconSize.MENU)
+        self.stop_image = Gtk.Image.new_from_icon_name("gtk-media-stop", Gtk.IconSize.MENU)
+
+        # setting the default image for the buttons
+        self.playback_button.set_image(self.play_image)
+        self.stop_button.set_image(self.stop_image)
+
     def show(self):
         self.show_all()
 
     def setup_objects_and_events(self):
-        self.playback_button = Gtk.Button()
-
-        self.stop_button = Gtk.Button()
-
-        self.play_image = Gtk.Image.new_from_icon_name("gtk-media-play", Gtk.IconSize.MENU)
-
-        self.pause_image = Gtk.Image.new_from_icon_name("gtk-media-pause", Gtk.IconSize.MENU)
-
-        self.stop_image = Gtk.Image.new_from_icon_name("gtk-media-stop", Gtk.IconSize.MENU)
-
-        self.playback_button.set_image(self.play_image)
-
-        self.stop_button.set_image(self.stop_image)
 
         self.playback_button.connect("clicked", self.toggle_player_playback)
 
@@ -43,6 +43,7 @@ class ApplicationWindow(Gtk.Window):
 
         self.draw_area.connect("realize", self._realized)
 
+        # virtual horizontal box which contains the both media playback buttons
         self.hbox = Gtk.Box(spacing=6)
         self.hbox.pack_start(self.playback_button, True, True, 0)
         self.hbox.pack_start(self.stop_button, True, True, 0)
@@ -59,21 +60,18 @@ class ApplicationWindow(Gtk.Window):
 
     def toggle_player_playback(self, widget, data=None):
 
-        """
-        Handler for Player's Playback Button (Play/Pause).
-        """
-
+        # if player is not active and is not paused
         if self.is_player_active == False and self.player_paused == False:
             self.player.play()
             self.playback_button.set_image(self.pause_image)
             self.is_player_active = True
 
-        elif self.is_player_active == True and self.player_paused == True:
+        elif self.is_player_active and self.player_paused:
             self.player.play()
             self.playback_button.set_image(self.pause_image)
             self.player_paused = False
 
-        elif self.is_player_active == True and self.player_paused == False:
+        elif self.is_player_active and self.player_paused == False:
             self.player.pause()
             self.playback_button.set_image(self.play_image)
             self.player_paused = True
@@ -96,5 +94,9 @@ window = ApplicationWindow()
 window.setup_objects_and_events()
 window.show()
 Gtk.main()
-window.player.stop()
-window.vlcInstance.release()
+while True:
+    # flag = wb.detect_face()
+    flag = True
+
+    window.player.stop()
+    window.vlcInstance.release()
