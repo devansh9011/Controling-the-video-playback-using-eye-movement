80266d3901f95a3e50ab284edf81fc80407d677c
Devansh Sharma <devansh9011@gmail.com>
2020-12-13 01:10:36 +0530

    implementing play pause in video player

--- test3.py
+++ test3.py
@@ -1,10 +1,16 @@
 import gi
 import vlc
+import cv2
+import numpy as np
+import time
+from threading import Thread
 
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
 # from gi.repository import GdkX11
+face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
 
 class ApplicationWindow(Gtk.Window):
@@ -78,6 +84,18 @@ class ApplicationWindow(Gtk.Window):
         else:
             pass
 
+    def pause_player(self, widget, data=None):
+        if self.is_player_active:
+            self.player.pause()
+            self.playback_button.set_image(self.play_image)
+            self.player_paused = True
+
+    def resume_player(self, widget, data=None):
+        if self.is_player_active:
+            self.player.play()
+            self.playback_button.set_image(self.pause_image)
+            self.player_paused = False
+
     def _realized(self, widget, data=None):
         self.vlcInstance = vlc.Instance("--no-xlib")
         self.player = self.vlcInstance.media_player_new()
@@ -89,14 +107,66 @@ class ApplicationWindow(Gtk.Window):
         self.is_player_active = True
 
 
+class webcam:
+    def __init__(self):
+        self.cap = cv2.VideoCapture(0)
+
+    def detect_face(self):
+        ret, frame = self.cap.read()
+        frame = cv2.add(frame, np.array([30.0]))  # adding brightness to the frame
+        face_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # converting the coloured image into black and white
+        detected = False
+        face_rectangle = face_cascade.detectMultiScale(face_img, scaleFactor=1.4, minNeighbors=5)  # detecting for face
+        for (x, y, w, h) in face_rectangle:
+            cv2.rectangle(face_img, (x, y), (x + w, y + h), (255, 255, 255),
+                          5)  # drawing rectangle over the detected face
+            detected = True
+            face = face_img[y:y + h, x:x + w]  # cropping the face rectangle
+            eyes_rectangle = eye_cascade.detectMultiScale(face)  # detection for eyes in the face
+            for (xx, yy, ww, hh) in eyes_rectangle:
+                cv2.rectangle(face_img, (xx + x, y + yy), (x + xx + ww, y + yy + hh), (255, 0, 0),
+                              2)  # drawing rectangle over the detected eyes
+
+        cv2.imshow('face detect', face_img)
+        return detected
+
+    def __del__(self):
+        self.cap.release()
+
+
+def foo():
+    global seconds, window
+    window.show()
+    Gtk.main()
+    while True:
+        flag = wb.detect_face()
+        if not flag:
+            if not window.player_paused:
+                window.pause_player()
+            else:
+                if time.time() - seconds >= 20:
+                    window.player.stop()
+                    window.vlcInstance.release()
+        else:
+            if window.player_paused:
+                window.resume_player()
+            seconds = time.time()
+
+        window.player.stop()
+        window.vlcInstance.release()
+
+
 MRL = '/home/devil/Downloads/video.mp4'
 window = ApplicationWindow()
 window.setup_objects_and_events()
-window.show()
-Gtk.main()
-while True:
-    # flag = wb.detect_face()
-    flag = True
 
-    window.player.stop()
-    window.vlcInstance.release()
+seconds = time.time()
+wb = webcam()
+foo();
+# Thread(target=foo, args=()).start()
+
+# window.show()
+# Gtk.main()
+# window.player.stop()
+# window.vlcInstance.release()
+
